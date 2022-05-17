fbb9675761e98aceffab01f2259bddc41d38ae10
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-05-17 11:31:43 +0900

    반환값 수정

--- app/app.py
+++ app/app.py
@@ -12,24 +12,70 @@ import os
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 from flask import Flask, request
-from siamese import SiameseNetwork # 필수
+from siamese import SiameseNetwork  # 필수
 from convert_image import ConvertImageData
 
 app = Flask(__name__)
 
-def get_image():
-    img_byte = request.files['file_url'].read()
+'''
+detect -> mq2
+{
+  album_id : "",
+  original_image_url : "",
+  file_image : ""
+}
+
+siamese -> mq2
+{
+  album_id : "",
+  person_url : "",
+  person_username : "",
+  rate : "",
+  original_image_url : ""
+}
+'''
+
+
+def get_album_id():
+    data = str(request.form['data'])
+    data_to_json = json.loads(data)
+    return data_to_json['album_id']
+
+
+def get_original_image_url():
+    data = str(request.form['data'])
+    data_to_json = json.loads(data)
+    return data_to_json['original_image_url']
+
+
+def get_file_image():
+    img_byte = request.files['file_image'].read()
     data_io = io.BytesIO(img_byte)
     return data_io
+
+
+def make_response(album_id, person_url, person_username, rate, original_image_url):
+    res = {'album_id': album_id,
+           'person_url': person_url,
+           'person_username': person_username,
+           'rate': rate,
+           'original_image_url': original_image_url
+           }
+    return res
 
 
 def response(result_dict):
     # 정렬
     sorted_result = sorted(result_dict.items(), key=lambda item: item[1])
     # 정렬을 하면 list로 바뀌므로 다시 dictionary로 변환
-    dict_ = dict(sorted_result)
-    # dictionary를 json으로 변환
-    return json.dumps(dict_)
+    # dict_ = dict(sorted_result)
+
+    album_id = get_album_id()
+    person_url = sorted_result[0][0] # 넘어온 사진과 가장 비슷한 사용자
+    rate = sorted_result[0][1] # 비슷한 정도
+    person_username = person_url.split('/')[4]
+    original_image_url = get_original_image_url()
+    return json.dumps(make_response(album_id, person_url, person_username, rate, original_image_url))
 
 
 @app.route('/check')
@@ -43,7 +89,7 @@ def siamese():
     result_dict = dict()
 
     # spring server로 부터 받은 이미지를 튜플로 전환
-    image_tuple = (get_image(), 0)
+    image_tuple = (get_file_image(), 0)
 
     get_images = s3_con.get_s3_images()
 
@@ -77,6 +123,6 @@ def siamese():
 
 if __name__ == "__main__":
     dir = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/') + '/'
-    model = torch.load(dir+cf.MODEL_PATH)
+    model = torch.load(dir + cf.MODEL_PATH)
     # model.eval()
     app.run(debug=False, host="127.0.0.1", port=5000, threaded=True)
